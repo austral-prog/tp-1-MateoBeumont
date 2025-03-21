a207df002f8cd419609e79574dbb7ffc7e8d0a79
MateoBeumont <mbeumont@mail.austral.edu.ar>
2025-03-21 11:06:13 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,22 @@
 def math():
     a = 57
     b = 7
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+    Promedio = (a + b)/2
+
+    print(Promedio)
+
+    Entero = a//b
+
+    print(Entero)
+
+    Resto = a%b
+
+    print(Resto)
+
+    Division = a/b
+    
+    print(Division)
